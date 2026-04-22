74372a85bdd1d580b6703d8d2552600fb126cee8
amanda <zheng749@purdue.edu>
2026-04-22 17:59:36 -0400

    dsfg

--- src/functions.h
+++ src/functions.h
@@ -2,3 +2,24 @@
 
 void init_gpio();
 void gpio_callback(uint, uint32_t);
+
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include "pico/stdlib.h"
+#include "hardware/pwm.h"
+
+
+static int duty_cycle = 0;
+static int dir = 0;
+static int color = 0;
+
+void display_init_pins();
+void display_init_timer();
+void display_char_print(const char message[]);
+void keypad_init_pins();
+void keypad_init_timer();
+void init_wavetable(void);
+void set_freq(int chan, float f);
+extern KeyEvents kev;
+void drum_machine();
