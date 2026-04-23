2ae468ad8f69e4be5dcf6c2bcd93b482467150a2
Lihua Wang <isaackkunag@gmail.com>
2026-04-22 20:40:11 -0400

    init

--- src/init.c
+++ src/init.c
@@ -12,11 +12,10 @@ bool hit;
 void gpio_callback() { //tell us what button is pressed
     for(int i = 0; i < 5; i++)
     {
-        if(gpio_get_irq_event_mask(moles[i]) & GPIO_IRQ_EDGE_RISE)
+        if(gpio_get_irq_event_mask(moles[i]) & GPIO_IRQ_EDGE_RISE) //might need to get rid of edge rise
         {
             // Acknowledge event
             gpio_acknowledge_irq(moles[i], GPIO_IRQ_EDGE_RISE);
-
             pressed_mole = moles[i];
             hit = true;
         } 
@@ -33,11 +32,12 @@ void init_gpio() {
     }
     
     irq_set_enabled(IO_IRQ_BANK0, true);
-    irq_set_enabled(GPIO_IRQ_EDGE_RISE, true);
-    
+    //irq_set_enabled(GPIO_IRQ_EDGE_RISE, true);
+    gpio_set_irq_callback(&gpio_callback);
+
     for (int i = 0; i < 5; i++) {
         gpio_set_irq_enabled(moles[i], GPIO_IRQ_EDGE_RISE, true);
-        gpio_add_raw_irq_handler_masked((1u << moles[i]), gpio_callback);
+        //gpio_add_raw_irq_handler_masked((1u << moles[i]), gpio_callback);
     }
 
 
