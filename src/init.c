e0ddd635e4cbba7361c761e485a8626f995a6cd5
Lihua Wang <isaackkunag@gmail.com>
2026-04-23 11:42:27 -0400

    m

--- src/init.c
+++ src/init.c
@@ -6,8 +6,8 @@
 
 int moles[] = {3,5,7,9,11};
 int lights[] = {4,6,8,10,12};
-int pressed_mole;
-bool hit;
+volatile int pressed_mole;
+volatile bool hit;
 
 void gpio_callback() { //tell us what button is pressed
     for(int i = 0; i < 5; i++)
@@ -30,17 +30,20 @@ void init_gpio() {
         gpio_pull_up(moles[i]);
 
     }
-    
-    irq_set_enabled(IO_IRQ_BANK0, true);
-    //irq_set_enabled(GPIO_IRQ_EDGE_RISE, true);
-    //gpio_set_irq_callback(&gpio_callback);
-    uint32_t mask = 1u << 3 | 1u << 5 | 1u << 7 | 1u << 9 | 1u << 1;
-    gpio_add_raw_irq_handler_masked(mask, gpio_callback);
-
     for (int i = 0; i < 5; i++) {
         gpio_set_irq_enabled(moles[i], GPIO_IRQ_EDGE_RISE, true);
         //gpio_add_raw_irq_handler_masked((1u << moles[i]), gpio_callback);
     }
+
+    
+    //irq_set_enabled(GPIO_IRQ_EDGE_RISE, true);
+    //gpio_set_irq_callback(&gpio_callback);
+    uint32_t mask = 1u << 3 | 1u << 5 | 1u << 7 | 1u << 9 | 1u << 11;
+    gpio_add_raw_irq_handler_masked(mask, gpio_callback);
+
+    irq_set_enabled(IO_IRQ_BANK0, true);
+
+    
 
 
     for(int i = 0; i < 5; i++) {
